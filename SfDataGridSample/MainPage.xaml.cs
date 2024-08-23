8cd06e6b7a8aa4eeca81df4174b228afb10c7f06
SethupathyD <sethupathy.d@syncfusion.com>
2024-08-23 14:51:07 +0530

    Code updated.

---
+++ SfDataGridSample/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel;
+
+namespace SfDataGridSample
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Entry_HandlerChanged(object sender, EventArgs e)
+        {
+#if WINDOWS
+            if (sender is Entry entry && entry.Handler?.PlatformView is Microsoft.Maui.Platform.MauiPasswordTextBox textBox)
+            {
+                textBox.KeyDown += TextBox_KeyDown;
+            }
+#endif
+        }
+
+#if WINDOWS 
+        private void TextBox_KeyDown(object sender, Microsoft.UI.Xaml.Input.KeyRoutedEventArgs e)
+        {
+            if (e.Key == Windows.System.VirtualKey.Tab) 
+            {
+                DisplayAlert("Tab Pressed", "The Tab key was pressed", "OK");
+            }
+
+        }
+#endif
+    }
+
+}
